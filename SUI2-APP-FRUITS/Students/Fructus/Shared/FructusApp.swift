1945411315f8f8f534165912d9c7605f6aa6e647
Michael Hunt <mhunt429@gmail.com>
2020-12-03 13:11:30 -0500

    project setup

---
+++ SUI2-APP-FRUITS/Students/Fructus/Shared/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Shared
+//
+//  Created by Michael Hunt on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
